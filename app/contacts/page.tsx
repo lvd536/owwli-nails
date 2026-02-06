fd039bc9b06dd7f254939e40821a23d34d4d3e45
lvd536 <nbikiti@gmail.com>
2026-02-06 19:53:30 +0400

    - Implemented contacts page & del unused images

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,3 +1,81 @@
+import { contacts } from "@/consts/contacts";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function page() {
-    return <div>page</div>;
+    return (
+        <div className="flex max-md:flex-col w-full p-5 gap-6">
+            <div className="flex flex-col items-center justify-center gap-2 max-md:mb-5">
+                <p className="text-lg font-medium bg-stone-900/60 rounded-lg px-6 py-6 lg:py-11">
+                    Здесь вы можете выбрать удобный способ связи со мной. В
+                    личном чате можно задать любые вопросы, уточнить детали
+                    процедуры, подобрать дизайн или записаться на удобную дату.
+                    Я всегда стараюсь отвечать быстро и помогать с выбором, даже
+                    если вы пока просто присматриваетесь.
+                    <br />
+                    В личном канале я делюсь новостями, свободными окнами для
+                    записи и важной информацией для клиентов. А в канале с
+                    примерами работ вы найдёте актуальные дизайны, формы и идеи
+                    для вдохновения — можно сразу выбрать понравившийся вариант
+                    и отправить его мне в чат.
+                    <br />
+                    Так вам будет проще сориентироваться, подобрать стиль и
+                    связаться со мной в пару кликов.
+                </p>
+            </div>
+            <ul className="flex flex-wrap items-center justify-start max-md:gap-6 gap-2">
+                <li>
+                    <Link
+                        href={contacts.telegramNailsChannel.link}
+                        className="flex md:w-60 gap-4 items-center justify-between bg-stone-900/60 rounded-lg p-2 pr-4"
+                    >
+                        <Image
+                            src={contacts.telegramNailsChannel.icon}
+                            alt="Contact image"
+                            width={50}
+                            height={50}
+                            className="p-1 bg-stone-900/60 rounded-lg"
+                        />
+                        <p className="text-md font-medium">
+                            {contacts.telegramNailsChannel.label}
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href={contacts.telegramPerson.link}
+                        className="flex md:w-60 gap-4 items-center justify-between bg-stone-900/60 rounded-lg p-2 pr-4"
+                    >
+                        <Image
+                            src={contacts.telegramPerson.icon}
+                            alt=""
+                            width={50}
+                            height={50}
+                            className="p-1 bg-stone-900/60 rounded-lg"
+                        />
+                        <p className="text-md font-medium">
+                            {contacts.telegramPerson.label}
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href={contacts.telegramPersonChannel.link}
+                        className="flex md:w-60 gap-4 items-center justify-between bg-stone-900/60 rounded-lg p-2 pr-4"
+                    >
+                        <Image
+                            src={contacts.telegramPersonChannel.icon}
+                            alt=""
+                            width={50}
+                            height={50}
+                            className="p-1 bg-stone-900/60 rounded-lg"
+                        />
+                        <p className="text-md font-medium">
+                            {contacts.telegramPersonChannel.label}
+                        </p>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
--- consts/contacts.ts
+++ consts/contacts.ts
@@ -5,17 +5,17 @@ import Girl from "@/public/girl.png";
 export const contacts = {
     telegramPerson: {
         label: "Личные сообщения",
-        link: "t.me/oliweeshka",
+        link: "https://t.me/oliweeshka",
         icon: Chat,
     },
     telegramPersonChannel: {
         label: "Личный Канал",
-        link: "t.me/owwww567",
+        link: "https://t.me/owwww567",
         icon: Girl,
     },
     telegramNailsChannel: {
         label: "Примеры работ",
-        link: "t.me/owwwwwli",
+        link: "https://t.me/owwwwwli",
         icon: Brush,
     },
 } as const;
